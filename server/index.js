23eed19b0dcf1a888294ecce744fdf452fdfa946
Celia Gomez <celia.gomez@unumotors.com>
2021-02-02 15:41:46 +0100

    chore: update server port

--- server/index.js
+++ server/index.js
@@ -6,6 +6,6 @@ const app = express()
 app.use(cors())
 graphql.applyMiddleware({ app, path: '/graphql' })
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 4000
 app.listen(port)
 console.log(`Running a GraphQL API server at localhost:${port}/graphql`)
