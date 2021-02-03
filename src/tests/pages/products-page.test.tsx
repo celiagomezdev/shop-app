0c09fbc8aeec1b1d0aeda762702cf3750689d66d
Celia Gomez <celia.gomez@unumotors.com>
2021-02-03 11:25:43 +0100

    test: add product card test

--- src/tests/pages/products-page.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import ProductsPage from '../../pages/products-page'
-
-test('renders products list header', () => {
-  render(<ProductsPage />)
-  const headerElement = screen.getByText(/products catalog/i)
-  expect(headerElement).toBeInTheDocument()
-})
