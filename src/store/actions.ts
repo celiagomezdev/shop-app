b0ecd0dafcdc24fb9c005c06826a7e42216cea3d
Celia Gomez <celia.gomez@unumotors.com>
2021-02-10 17:20:44 +0100

    refactor: change store types

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,6 +1,7 @@
-import { Item, CartActionTypes, ADD_ITEM_TO_CART } from './types'
+import { CartActionTypes, ADD_ITEM_TO_CART } from './types'
+import { Product } from '../__generated__/types'
 
-export function addItemToCart(newItem: Item): CartActionTypes {
+export function addItemToCart(newItem: Product): CartActionTypes {
   return {
     type: ADD_ITEM_TO_CART,
     payload: newItem,
